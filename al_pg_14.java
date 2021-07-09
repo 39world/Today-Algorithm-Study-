c9db19a9c793a691acc56fa9f83e22dc2b65a00b
Sanggu <rkdtkdrn93@naver.com>
2021-07-09 23:56:39 +0900

    today_al
    today_al

--- al_pg_14.java
+++ al_pg_14.java
@@ -1,16 +1,42 @@
-class Solution {
-  public String solution(String str) {
-      
-        String[] tmp = str.split(" ");
-        int min, max, n;
-        min = max = Integer.parseInt(tmp[0]);
-        for (int i = 1; i < tmp.length; i++) {
-                n = Integer.parseInt(tmp[i]);
-            if(min > n) min = n;
-            if(max < n) max = n;
-        }
+import java.util.Scanner;
  
-        return min + " " + max;
+public class Main{
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int N = sc.nextInt();
  
-  }
-}
+        int [][] arr = new int[N][N];
+         int x = -1 , y = -1;
+         int num = 0;
+         for(int i = 0 ; i < N; ++i)
+         {
+             for(int j = i; j < N; ++j)
+             {
+                 if(i%3 == 0)
+                 {
+                     x++; y++;
+                 }else if(i%3 == 1)
+                 {
+                     y--;
+                 }else if(i%3 == 2)
+                 {
+                     x--;
+                 }
+                 arr[x][y] = (num++)%10;
+             }
+         }
+ 
+ 
+         for(int i = 0; i < N; ++i)
+         {
+             for(int j = 0; j < i+1; ++j)
+             {
+                 System.out.print(arr[i][j]+ " ");
+             }System.out.println();
+         }
+ 
+    }
+ }
+
+
+출처: https://dyndy.tistory.com/82 [DY N DY]
