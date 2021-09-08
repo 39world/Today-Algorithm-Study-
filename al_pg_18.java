f72886cbfbad5219bee1caa8778da2f42e07418c
Sanggu <rkdtkdrn93@naver.com>
2021-09-08 23:09:52 +0900

    today_al
    today_al

---
+++ al_pg_18.java
@@ -0,0 +1,18 @@
+#프로그래머스 2xn 타일링 
+
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] array = new int[n+1];
+        array[1] = 1;
+        array[2] = 2;
+        for(int i = 3 ; i<=n ; i++){
+            array[i] = (array[i-1]+array[i-2])%1000000007;
+        }
+        answer = array[n];
+            
+        return answer;
+    }
+}
